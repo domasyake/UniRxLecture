8867129160cd75e9ae5644c8bf4648fe945c57e8
domasyake <lehmboden.doma@gmail.com>
2019-03-09 18:54:04 +0900

    add:where ex

---
+++ Assets/Chapter4/OperatorExample/WhereExample.cs
@@ -0,0 +1,13 @@
+﻿using Chapter2.Event;
+using UniRx;
+using UnityEngine;
+
+namespace Chapter4.OperatorExample{
+	public class WhereExample : MonoBehaviour{
+		private void Start (){
+			this.GetComponent<SecondsCounterRx>().OnCountUp
+				.Where(n=>n%2==0)//流れてきたイベントの値が偶数の時のみ通す
+				.Subscribe(n => Debug.Log("Hi,im whereEx.count is "+n));
+		}
+	}
+}
