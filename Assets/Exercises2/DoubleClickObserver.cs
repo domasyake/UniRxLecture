888eefe5d22eb4ec8baca03427e49fd4d6158c90
domasyake <lehmboden.doma@gmail.com>
2019-03-10 02:01:43 +0900

    add:exercises2

---
+++ Assets/Exercises2/DoubleClickObserver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+namespace Exercises2{
+	public class DoubleClickObserver : MonoBehaviour {
+
+		private readonly Subject<Unit> clickCheckStream=new Subject<Unit>();//自分の判定に使うだけなのでprivate
+		
+		private void Start (){
+			//オペレータでほにゃららしてダブルクリック判定する
+			clickCheckStream
+				.Subscribe(n => Debug.Log("Double click!"));
+		}
+	
+		private void Update () {
+			if (Input.GetMouseButtonDown(0)){
+				//イベント発行
+			}
+		}
+	}
+}
