d2e6e32c9188289d199e82ef94c3c2581ad885ed
domasyake <lehmboden.doma@gmail.com>
2019-03-10 14:12:05 +0900

    fix:change check logic to exact

--- Assets/Exercises2/DoubleClickObserver.cs
+++ Assets/Exercises2/DoubleClickObserver.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using UniRx;
 using UnityEngine;
 
@@ -8,8 +9,13 @@ namespace Exercises2{
 		private readonly Subject<Unit> clickCheckStream=new Subject<Unit>();//自分の判定に使うだけなのでprivate
 		
 		private void Start (){
-			//オペレータでほにゃららしてダブルクリック判定する
+			var double_click_interval = 0.25d;//ダブルクリックのインターバル
+		
 			clickCheckStream
+				.TimeInterval()//直前のイベント発行からの時間差分ストリームに変換
+				.Select(t => t.Interval.TotalSeconds)//差分の秒数ストリームに変換
+				//イベントを纏める
+				//インターバル以内にクリックされているかチェック
 				.Subscribe(n => Debug.Log("Double click!"));
 		}
 	
